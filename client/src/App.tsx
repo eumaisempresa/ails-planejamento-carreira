610813930a510adf699c5c83d428382c0c846698
eumaisempresa <2.86512028e+08+eumaisempresa@users.noreply.github.com>
2026-09-04 15:15:01 +0000

    Support GitHub Pages subpath

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/ails-planejamento-carreira/" component={Home} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
